27eb8235e1c59c36b801b08b3061b016200afcd1
Jankovv295 <jankov295@gmail.com>
2023-05-09 21:59:21 +0200

    Homework

---
+++ 4.Advanced LINQ/ConsoleApp1/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1.Entities
+{
+    public abstract class BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+    }
+}
